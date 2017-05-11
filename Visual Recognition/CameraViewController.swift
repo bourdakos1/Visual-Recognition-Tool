de529054594bee37454a7e97f4b54a72deb95450
Nick Bourdakos <bourdakos1@gmail.com>
2017-05-11 13:06:21 -0700

    start cleanup

--- Visual Recognition/CameraViewController.swift
+++ Visual Recognition/CameraViewController.swift
@@ -10,18 +10,33 @@ import UIKit
 import AVFoundation
 
 class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, AVCapturePhotoCaptureDelegate {
+    
+    // Set the StatusBar color.
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
+    // Demo API key constant.
     let VISION_API_KEY: String
     
+    // Camera variables.
     var captureSession: AVCaptureSession?
     var photoOutput: AVCapturePhotoOutput?
     var previewLayer: AVCaptureVideoPreviewLayer?
     @IBOutlet var cameraView: UIView!
     @IBOutlet var tempImageView: UIImageView!
     
+    // All the buttons.
+    @IBOutlet var classifiersButton: UIButton!
+    @IBOutlet var captureButton: UIButton!
+    @IBOutlet var retakeButton: UIButton!
+    @IBOutlet var apiKeyDoneButton: UIButton!
+    @IBOutlet var apiKey: UIButton!
+    @IBOutlet var apiKeySubmit: UIButton!
+    @IBOutlet var apiKeyTextField: UITextField!
+    @IBOutlet var hintTextView: UITextView!
+    
+    // Init with the demo API key.
     required init?(coder aDecoder: NSCoder) {
         var keys: NSDictionary?
         
@@ -36,11 +51,14 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Look in user defaults to see if we have a real key.
         var apiKeyText = UserDefaults.standard.string(forKey: "api_key")
         
         if apiKeyText == nil || apiKeyText == "" {
+            // If we don't have a key set the text of the bar to "API Key".
             apiKeyText = "🔑 API Key"
         } else {
+            // If we do have a key chop it up and hide the center.
             let a = apiKeyText![apiKeyText!.index(apiKeyText!.startIndex, offsetBy: 0)]
             
             let start = apiKeyText!.index(apiKeyText!.endIndex, offsetBy: -3)
@@ -50,27 +68,36 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
             apiKeyText = "🔑 \(a)•••••••••••••••••••••••••••••••••••••\(b)"
         }
         
+        // Style the API text.
         apiKey.layer.shadowOffset = CGSize(width: 0, height: 1)
         apiKey.layer.shadowOpacity = 0.2
         apiKey.layer.shadowRadius = 5
         apiKey.layer.masksToBounds = false
         apiKey.setAttributedTitle(NSAttributedString(string: apiKeyText!, attributes: [NSForegroundColorAttributeName : UIColor.white, NSStrokeColorAttributeName : UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0), NSStrokeWidthAttributeName : -0.5]), for: .normal)
-        apiKeyTextField.attributedPlaceholder = NSAttributedString(string: "API Key", attributes: [NSForegroundColorAttributeName: UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)])
         
+        // Give the API TextField styles and a stroke.
+        apiKeyTextField.attributedPlaceholder = NSAttributedString(string: "API Key", attributes: [NSForegroundColorAttributeName: UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)])
         apiKeyTextField.setLeftPaddingPoints(20)
         apiKeyTextField.setRightPaddingPoints(50)
+        
+        initializeCamera()
 
+        // Start out with capture button shown
+        captureButton.isHidden = false
+        hideRetake()
+        view.bringSubview(toFront: captureButton)
+    }
+    
+    // Initialize camera.
+    func initializeCamera() {
         captureSession = AVCaptureSession()
         captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
-        
         let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
         
         do {
             let input = try AVCaptureDeviceInput(device: backCamera)
             captureSession?.addInput(input)
-            
             photoOutput = AVCapturePhotoOutput()
-            
             if (captureSession?.canAddOutput(photoOutput) != nil){
                 captureSession?.addOutput(photoOutput)
                 
@@ -87,15 +114,12 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
                 captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
             }
         } catch {
-            print("error")
+            print("Error: \(error)")
         }
         previewLayer?.frame = view.bounds
-        
-        captureButton.isHidden = false
-        hideRetake()
-        view.bringSubview(toFront: captureButton)
     }
     
+    // Delegate for QR Codes.
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
         // Check if the metadataObjects array is not nil and it contains at least one object.
         if metadataObjects == nil || metadataObjects.count == 0 {
@@ -113,63 +137,57 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
         testKey(key: qrCode)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
-    func didPressTakePhoto() {
-        photoOutput?.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
-    }
-    
+    // Delegate for Camera.
     func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
         
+        var apiKey = UserDefaults.standard.string(forKey: "api_key")
+        
+        if apiKey == nil || apiKey == "" {
+            apiKey = VISION_API_KEY
+        }
+        
         if photoSampleBuffer != nil {
-            let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(
+                forJPEGSampleBuffer: photoSampleBuffer!,
+                previewPhotoSampleBuffer: previewPhotoSampleBuffer
+            )
+            
             let dataProvider  = CGDataProvider(data: imageData! as CFData)
-            let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
-
-            let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
-
-            let image2 = image.resized(toWidth: 300)!
-
-            print("dimensions : \(image2.size)")
-            print("size : \(UIImageJPEGRepresentation(image2, 0.4)!.count)")
-
-            var apiKey = UserDefaults.standard.string(forKey: "api_key")
-
-            if apiKey == nil || apiKey == "" {
-                apiKey = self.VISION_API_KEY
-            }
-
-            let escapedApiKey = apiKey?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-
-
-            /// UPLOAD
-
+            
+            let cgImageRef = CGImage(
+                jpegDataProviderSource: dataProvider!,
+                decode: nil,
+                shouldInterpolate: true,
+                intent: .defaultIntent
+            )
+            
+            let image = UIImage(
+                cgImage: cgImageRef!,
+                scale: 1.0,
+                orientation: UIImageOrientation.right
+            )
+            
+            let reducedImage = image.resized(toWidth: 300)!
+            
             let classifierId = UserDefaults.standard.string(forKey: "classifier_id")
-
-            print(escapedApiKey!)
-
-            var r  = URLRequest(url: URL(string: "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=\(escapedApiKey!)&version=2016-05-20&threshold=0&classifier_ids=\(classifierId ?? "default")")!)
-            r.httpMethod = "POST"
-            let boundary = "Boundary-\(UUID().uuidString)"
-            r.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
-            r.httpBody = self.createBody(parameters: [:],
-                                    boundary: boundary,
-                                    data: UIImageJPEGRepresentation(image2, 0.4)!,
-                                    mimeType: "image/jpg",
-                                    filename: "hello.jpg")
-
+            
+            let url = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify"
+            
+            var r = URLRequest(url: URL(string: url)!)
+            
+            r.query(params: [
+                "api_key": apiKey!,
+                "version": "2016-05-20",
+                "threshold": "0",
+                "classifier_ids": "\(classifierId ?? "default")"
+            ])
+            
+            // Attach the small image at 40% quality.
+            r.attach(
+                jpeg: UIImageJPEGRepresentation(reducedImage, 0.4)!,
+                filename: "test.jpg"
+            )
+            
             let task = URLSession.shared.dataTask(with: r) { data, response, error in
                 guard let data = data, error == nil else {               // check for fundamental networking error
                     return
@@ -197,34 +215,13 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
             }
             task.resume()
             
-            self.tempImageView.image = image
-            self.tempImageView.isHidden = false
+            tempImageView.image = image
+            tempImageView.isHidden = false
         }
     }
     
-    func createBody(parameters: [String: String],
-                    boundary: String,
-                    data: Data,
-                    mimeType: String,
-                    filename: String) -> Data {
-        let body = NSMutableData()
-        
-        let boundaryPrefix = "--\(boundary)\r\n"
-        
-        for (key, value) in parameters {
-            body.appendString(boundaryPrefix)
-            body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
-            body.appendString("\(value)\r\n")
-        }
-        
-        body.appendString(boundaryPrefix)
-        body.appendString("Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n")
-        body.appendString("Content-Type: \(mimeType)\r\n\r\n")
-        body.append(data)
-        body.appendString("\r\n")
-        body.appendString("--".appending(boundary.appending("--")))
-        
-        return body as Data
+    func didPressTakePhoto() {
+        photoOutput?.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
     }
     
     var didTakePhoto = Bool()
@@ -295,58 +292,7 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
         apiKey.isHidden = false
     }
     
-    
-    @IBOutlet var classifiersButton: UIButton!
-    @IBOutlet var captureButton: UIButton!
-    @IBOutlet var retakeButton: UIButton!
-    @IBOutlet var apiKeyDoneButton: UIButton!
-    @IBOutlet var apiKey: UIButton!
-    @IBOutlet var apiKeySubmit: UIButton!
-    @IBOutlet var apiKeyTextField: UITextField!
-    @IBOutlet var hintTextView: UITextView!
-    
     let blurredEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-    
-    @IBAction func unwindToVC(segue: UIStoryboardSegue) {
-
-    }
-    
-    @IBAction func addApiKey() {
-        blurredEffectView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
-        view.addSubview(blurredEffectView)
-        
-        apiKeyTextField.isHidden = false
-        apiKeyTextField.becomeFirstResponder()
-        view.addSubview(apiKeyTextField)
-        
-        apiKeyDoneButton.isHidden = false
-        view.addSubview(apiKeyDoneButton)
-        
-        apiKeySubmit.isHidden = false
-        view.addSubview(apiKeySubmit)
-        
-        
-        hintTextView.isHidden = false
-        view.addSubview(hintTextView)
-        print("add view")
-    }
-    
-    @IBAction func apiKeyDone() {
-        apiKeyTextField.isHidden = true
-        apiKeyDoneButton.isHidden = true
-        apiKeySubmit.isHidden = true
-        hintTextView.isHidden = true
-        
-        blurredEffectView.removeFromSuperview()
-        view.endEditing(true)
-        apiKeyTextField.text = ""
-        print("remove view")
-    }
-    
-    @IBAction func submitApiKey() {
-        let key = apiKeyTextField.text
-        testKey(key: key!)
-    }
     
     func testKey(key: String) {
         var key = key.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
@@ -410,6 +356,47 @@ class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
         task.resume()
     }
     
+    @IBAction func unwindToVC(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func addApiKey() {
+        blurredEffectView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        view.addSubview(blurredEffectView)
+        
+        apiKeyTextField.isHidden = false
+        apiKeyTextField.becomeFirstResponder()
+        view.addSubview(apiKeyTextField)
+        
+        apiKeyDoneButton.isHidden = false
+        view.addSubview(apiKeyDoneButton)
+        
+        apiKeySubmit.isHidden = false
+        view.addSubview(apiKeySubmit)
+        
+        
+        hintTextView.isHidden = false
+        view.addSubview(hintTextView)
+        print("add view")
+    }
+    
+    @IBAction func apiKeyDone() {
+        apiKeyTextField.isHidden = true
+        apiKeyDoneButton.isHidden = true
+        apiKeySubmit.isHidden = true
+        hintTextView.isHidden = true
+        
+        blurredEffectView.removeFromSuperview()
+        view.endEditing(true)
+        apiKeyTextField.text = ""
+        print("remove view")
+    }
+    
+    @IBAction func submitApiKey() {
+        let key = apiKeyTextField.text
+        testKey(key: key!)
+    }
+    
     @IBAction func takePic() {
         didPressTakeAnother()
     }
@@ -426,6 +413,67 @@ extension NSMutableData {
     }
 }
 
+extension Dictionary {
+    func toHttpParameters() -> String {
+        let parameterArray = self.map { (key, value) -> String in
+            let percentEscapedKey = (key as! String).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            let percentEscapedValue = (value as! String).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            return "\(percentEscapedKey!)=\(percentEscapedValue!)"
+        }
+        return parameterArray.joined(separator: "&")
+    }
+}
+
+extension URLRequest {
+    mutating func attach(jpeg: Data, filename: String) {
+        httpMethod = "POST"
+        let boundary = "Boundary-\(UUID().uuidString)"
+        setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        httpBody = createBody(
+            parameters: [:],
+            boundary: boundary,
+            data: jpeg,
+            mimeType: "image/jpg",
+            filename: filename
+        )
+    }
+    
+    mutating func query(params: [String: String]) {
+        let parameterArray = params.map { (key, value) -> String in
+            let percentEscapedKey = key.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            let percentEscapedValue = value.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+            return "\(percentEscapedKey!)=\(percentEscapedValue!)"
+        }
+        let baseUrl = (url?.absoluteString)!
+        url = URL(string: "\(baseUrl)?\(parameterArray.joined(separator: "&"))")
+    }
+    
+    private func createBody(parameters: [String: String],
+                    boundary: String,
+                    data: Data,
+                    mimeType: String,
+                    filename: String) -> Data {
+        let body = NSMutableData()
+        
+        let boundaryPrefix = "--\(boundary)\r\n"
+        
+        for (key, value) in parameters {
+            body.appendString(boundaryPrefix)
+            body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+            body.appendString("\(value)\r\n")
+        }
+        
+        body.appendString(boundaryPrefix)
+        body.appendString("Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n")
+        body.appendString("Content-Type: \(mimeType)\r\n\r\n")
+        body.append(data)
+        body.appendString("\r\n")
+        body.appendString("--".appending(boundary.appending("--")))
+        
+        return body as Data
+    }
+}
+
 extension UIImage {
     func resized(withPercentage percentage: CGFloat) -> UIImage? {
         let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
@@ -434,6 +482,7 @@ extension UIImage {
         draw(in: CGRect(origin: .zero, size: canvasSize))
         return UIGraphicsGetImageFromCurrentImageContext()
     }
+    
     func resized(toWidth width: CGFloat) -> UIImage? {
         let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
         UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
@@ -449,6 +498,7 @@ extension UITextField {
         self.leftView = paddingView
         self.leftViewMode = .always
     }
+    
     func setRightPaddingPoints(_ amount:CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.rightView = paddingView
