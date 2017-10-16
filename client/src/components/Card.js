4f4aa538d5a358f77594b94a1016b960c81d0752
Nick Bourdakos <bourdakos1@gmail.com>
2017-10-16 19:03:48 -0400

    basic styles

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './styles/Card.css'
 
 const Card = ({ children }) => {
-  return <div>{children}</div>
+  return <div className="Card">{children}</div>
 }
 
 export default Card
--- client/src/components/ClassifierDetail.js
+++ client/src/components/ClassifierDetail.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import './styles/ClassifierDetail.css'
+import 'styles/fonts.css'
 import Card from 'components/Card'
 import StatusIndicator from 'components/StatusIndicator'
 
 const ClassifierDetail = ({ name, classifierId, status }) => {
   return (
     <Card>
-      <div>{name}</div>
-      <div>{classifierId}</div>
+      <div className="ClassifierDetail-text ClassifierDetail-name font-header">
+        {name}
+      </div>
+      <div className="ClassifierDetail-text ClassifierDetail-classifier-id font-default">
+        {classifierId}
+      </div>
       <StatusIndicator status={status} />
     </Card>
   )
