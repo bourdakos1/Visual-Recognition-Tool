64f7ef9570e593210feba65af9aa85f43b4856c7
Nick Bourdakos <bourdakos1@gmail.com>
2017-12-17 11:42:00 -0500

    commit incase im breaking too much

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -8,43 +8,20 @@ import user from 'images/user.svg'
 
 const TitleBar = ({ apiKey, children }) => (
   <div>
-    <div className="TitleBar-status">
-      <div className="TitleBar-user-container">
-        <img src={user} className="TitleBar-user" alt="User" />
-      </div>
-    </div>
     <div className="TitleBar-shadow">
-      <div className="TitleBar-contentWrapper">
+      <div className="TitleBar-left">
         <Link to="/" className="TitleBar-link">
           <img src={logo} className="TitleBar-logo" alt="Logo" />
-          <h1 className="TitleBar-h1">IBM</h1>
-          <span className="TitleBar-span">Watson</span>
+          <h1 className="TitleBar-h1">
+            IBM <span className="TitleBar-span">Cloud</span>
+          </h1>
         </Link>
 
-        <h4 className="TitleBar-h4">Visual Recognition</h4>
+        <h4 className="TitleBar-h4">Docs</h4>
+      </div>
 
-        <Link to="/">
-          <div className="TitleBar-doc-item">Docs</div>
-        </Link>
+      <div className="TitleBar-right">
 
-        <Link to="/">
-          <div className="TitleBar-doc-item">API Reference</div>
-        </Link>
-        {
-          // <div className="TitleBar-api-key font-default">
-          //   <button className="TitleBar-key-button">
-          //     <span role="img" aria-label="Key">
-          //       🔑
-          //     </span>
-          //   </button>
-          //
-          //   {apiKey.slice(0, 1)}
-          //   <span id="TitleBar-key-middle">{apiKey.slice(1, -3)}</span>
-          //   {apiKey.slice(-3)}
-          // </div>
-          //
-          // <Button className="TitleBar-button">Update key</Button>
-        }
       </div>
     </div>
     <div className="TitleBar-height-blank" />
--- server.js
+++ server.js
@@ -20,49 +20,49 @@ app.get('/api/classifiers', function(req, res) {
 })
 
 // Multer config
-const upload = multer({
-  limits: {
-    files: 1,
-    fileSize: 2 * 1024 * 1024 // 2mb
-  },
-  fileFilter: function(req, file, cb) {
-    var type = file.mimetype
-    if (type !== 'image/png' && type !== 'image/jpg' && type !== 'image/jpeg') {
-      cb(new Error('Invalid image type'))
-    } else {
-      cb(null, true)
-    }
-  },
-  storage: storage
-})
-
-var fileUpload = upload.single('file')
-app.post('/api/classify', function(req, res) {
-  fileUpload(req, res, function(err) {
-    if (err) {
-      res.send(err)
-      return
-    }
-
-    var visual_recognition = new VisualRecognitionV3({
-      api_key: req.query.api_key,
-      version_date: req.query.version || '2016-05-19'
-    })
-
-    var params = req.query
-
-    params.images_file = fs.createReadStream(req.file.path)
-
-    visual_recognition.classify(params, function(err, data) {
-      fs.unlinkSync(req.file.path)
-      if (err) {
-        res.send(err)
-        return
-      }
-      res.send(data)
-    })
-  })
-})
+// const upload = multer({
+//   limits: {
+//     files: 1,
+//     fileSize: 2 * 1024 * 1024 // 2mb
+//   },
+//   fileFilter: function(req, file, cb) {
+//     var type = file.mimetype
+//     if (type !== 'image/png' && type !== 'image/jpg' && type !== 'image/jpeg') {
+//       cb(new Error('Invalid image type'))
+//     } else {
+//       cb(null, true)
+//     }
+//   },
+//   storage: storage
+// })
+//
+// var fileUpload = upload.single('file')
+// app.post('/api/classify', function(req, res) {
+//   fileUpload(req, res, function(err) {
+//     if (err) {
+//       res.send(err)
+//       return
+//     }
+//
+//     var visual_recognition = new VisualRecognitionV3({
+//       api_key: req.query.api_key,
+//       version_date: req.query.version || '2016-05-19'
+//     })
+//
+//     var params = req.query
+//
+//     params.images_file = fs.createReadStream(req.file.path)
+//
+//     visual_recognition.classify(params, function(err, data) {
+//       fs.unlinkSync(req.file.path)
+//       if (err) {
+//         res.send(err)
+//         return
+//       }
+//       res.send(data)
+//     })
+//   })
+// })
 
 const port = process.env.PORT || 3001
 app.listen(port, function() {
