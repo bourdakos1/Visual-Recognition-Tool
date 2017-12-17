cf04b68be6c804b7bf586133d39a1e1c1ee7b04f
Nick Bourdakos <bourdakos1@gmail.com>
2017-12-17 16:38:26 -0500

    finally aligned

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -10,14 +10,19 @@ const TitleBar = ({ apiKey, children }) => (
   <div>
     <div className="TitleBar-shadow">
       <div className="TitleBar-left">
-        <Link to="/" className="TitleBar-link">
-          <img src={logo} className="TitleBar-logo" alt="Logo" />
-          <h1 className="TitleBar-h1">
-            IBM <span className="TitleBar-span">Cloud</span>
-          </h1>
-        </Link>
 
-        <h4 className="TitleBar-h4">Docs</h4>
+        <Link to="/" className="TitleBar-link-logo">
+          <img src={logo} className="TitleBar-logo" alt="Logo" />
+        </Link>
+        <div className="TitleBar-text-wrapper">
+          <Link to="/" className="TitleBar-link">
+            <h1 className="TitleBar-h1">
+              IBM <span className="TitleBar-span">Watson</span>
+            </h1>
+          </Link>
+
+          <h4 className="TitleBar-h4">Visual Recognition</h4>
+        </div>
       </div>
 
       <div className="TitleBar-right">
