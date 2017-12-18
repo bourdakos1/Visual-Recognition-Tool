83216fde67ea00d52b7b6f51febd9d6dff690259
Nick Bourdakos <bourdakos1@gmail.com>
2017-12-18 10:43:54 -0500

    add logout button

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import './styles/TitleBar.css'
@@ -6,34 +6,84 @@ import 'styles/fonts.css'
 import logo from 'images/watson.svg'
 import user from 'images/user.svg'
 
-const TitleBar = ({ apiKey, children }) => (
-  <div>
-    <div className="TitleBar-shadow">
-      <div className="TitleBar-left">
-        <Link to="/" className="TitleBar-link-logo">
-          <img src={logo} className="TitleBar-logo" alt="Logo" />
-        </Link>
-        <div className="TitleBar-text-wrapper">
-          <Link to="/" className="TitleBar-link">
-            <h1 className="TitleBar-h1">
-              IBM <span className="TitleBar-span">Watson</span>
-            </h1>
-          </Link>
+class TitleBar extends Component {
+  state = {
+    showLogout: false
+  }
 
-          <h4 className="TitleBar-h4">Visual Recognition</h4>
+  handleClick = () => {
+    if (!this.state.showLogout) {
+      // attach/remove event handler
+      document.addEventListener('click', this.handleOutsideClick, false)
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false)
+    }
+
+    this.setState(prevState => ({
+      showLogout: !prevState.showLogout
+    }))
+  }
+
+  handleOutsideClick = e => {
+    // ignore clicks on the component itself
+    // if (this.node.contains(e.target)) {
+    //   return
+    // }
+
+    this.handleClick()
+  }
+
+  render() {
+    const { apiKey, children } = this.props
+    return (
+      <div>
+        <div className="TitleBar-shadow">
+          <div className="TitleBar-left">
+            <Link to="/" className="TitleBar-link-logo">
+              <img src={logo} className="TitleBar-logo" alt="Logo" />
+            </Link>
+            <div className="TitleBar-text-wrapper">
+              <Link to="/" className="TitleBar-link">
+                <h1 className="TitleBar-h1">
+                  IBM <span className="TitleBar-span">Watson</span>
+                </h1>
+              </Link>
+
+              <h4 className="TitleBar-h4">Visual Recognition</h4>
+            </div>
+          </div>
+
+          <div className="TitleBar-right">
+            <Link to="/" className="TitleBar-overflow-item">
+              Docs
+            </Link>
+            <Link to="/" className="TitleBar-overflow-item">
+              API Reference
+            </Link>
+            <Link
+              to="/"
+              onClick={this.handleClick}
+              className={`TitleBar-overflow-item TitleBar-highlight ${this.state.showLogout
+                ? 'TitleBar-highlight-active'
+                : ''}`}
+            >
+              <img src={user} className="TitleBar-user" alt="User" />
+            </Link>
+            <div
+              className={`TitleBar-account-details ${this.state.showLogout
+                ? 'TitleBar-account-details-show'
+                : 'TitleBar-account-details-hide'}`}
+            >
+              <div className="TitleBar-section-logout">
+                <Link to="/" className="TitleBar-logout">Log Out</Link>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="TitleBar-height-blank" />
       </div>
-
-      <div className="TitleBar-right">
-        <Link to="/" className="TitleBar-overflow-item">Docs</Link>
-        <Link to="/" className="TitleBar-overflow-item">API Reference</Link>
-        <Link to="/" className="TitleBar-overflow-item TitleBar-highlight">
-          <img src={user} className="TitleBar-user" alt="User" />
-        </Link>
-      </div>
-    </div>
-    <div className="TitleBar-height-blank" />
-  </div>
-)
+    )
+  }
+}
 
 export default TitleBar
