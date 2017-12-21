a1b85181b2ab1ad2e6615b891b31c22a7c81e7e7
Nick Bourdakos <bourdakos1@gmail.com>
2017-12-21 15:57:10 -0500

    revamp dropdown

--- client/src/components/ThreeDotMenu.js
+++ client/src/components/ThreeDotMenu.js
@@ -1,40 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './styles/ThreeDotMenu.css'
 import 'styles/fonts.css'
 import dots from 'images/btn_dropdown.png'
 
-const ThreeDotMenu = () => {
-  function dropdown(e) {
-    console.log(e.target)
-    e.target.nextSibling.classList.toggle('ThreeDotMenu-show')
+class ThreeDotMenu extends Component {
+  state = {
+    showDropDown: false
   }
 
-  return (
-    <div className="ThreeDotMenu-dropdown">
-      <button
-        onClick={dropdown}
-        style={{ backgroundImage: `url('` + dots + `')` }}
-        className="ThreeDotMenu-dropbtn"
-      />
-      <div id="myDropdown" className="ThreeDotMenu-dropdownContent">
-        <a className="ThreeDotMenu-aStyle ThreeDotMenu-disabled" key="0" href="#">
-          Test
-        </a>
-        <a className="ThreeDotMenu-aStyle ThreeDotMenu-disabled" key="1" href="#">
-          Update
-        </a>
-        <div className="ThreeDotMenu-divider" />
-        <a
-          className="ThreeDotMenu-aStyle ThreeDotMenu-destructive"
-          key="2"
-          href="#"
+  handleClick = () => {
+    if (!this.state.showDropDown) {
+      // attach/remove event handler
+      document.addEventListener('click', this.handleOutsideClick, false)
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false)
+    }
+
+    this.setState(prevState => ({
+      showDropDown: !prevState.showDropDown
+    }))
+  }
+
+  handleOutsideClick = e => {
+    // Ignore clicks on the dropdown itself.
+    if (this.dropdownContent.contains(e.target)) {
+      return
+    }
+
+    this.handleClick()
+  }
+
+  // dropdown = e => {
+  //   e.target.nextSibling.classList.toggle('ThreeDotMenu-show')
+  // }
+
+  render() {
+    return (
+      <div className="ThreeDotMenu-dropdown">
+        <button
+          onClick={this.handleClick}
+          style={{ backgroundImage: `url('` + dots + `')` }}
+          className="ThreeDotMenu-dropbtn"
+        />
+        <div
+          className={`ThreeDotMenu-dropdownContent ${this.state.showDropDown &&
+            'ThreeDotMenu-show'}`}
+          ref={input => {
+            this.dropdownContent = input
+          }}
         >
-          Delete
-        </a>
+          <a
+            className="ThreeDotMenu-aStyle ThreeDotMenu-disabled"
+            key="0"
+            href="#"
+          >
+            Test
+          </a>
+          <a
+            className="ThreeDotMenu-aStyle ThreeDotMenu-disabled"
+            key="1"
+            href="#"
+          >
+            Update
+          </a>
+          <div className="ThreeDotMenu-divider" />
+          <a
+            className="ThreeDotMenu-aStyle ThreeDotMenu-destructive"
+            key="2"
+            href="#"
+          >
+            Delete
+          </a>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ThreeDotMenu
