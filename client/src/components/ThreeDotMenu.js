906d1eeb03aa22c6b765b88fd6b8843efcb190eb
Nick Bourdakos <bourdakos1@gmail.com>
2017-12-20 23:51:52 -0500

    update three dot menu

--- client/src/components/ThreeDotMenu.js
+++ client/src/components/ThreeDotMenu.js
@@ -6,24 +6,25 @@ import dots from 'images/btn_dropdown.png'
 
 const ThreeDotMenu = () => {
   function dropdown() {
-    document.getElementById('myDropdown').classList.toggle('show')
+    document.getElementById('myDropdown').classList.toggle('ThreeDotMenu-show')
   }
 
   return (
-    <div className="dropdown">
+    <div className="ThreeDotMenu-dropdown">
       <button
         onClick={dropdown}
         style={{ backgroundImage: `url('` + dots + `')` }}
-        className="dropbtn"
+        className="ThreeDotMenu-dropbtn"
       />
-      <div id="myDropdown" className="dropdownContent">
-        <a className="aStyle aa" key="0" href="#">
+      <div id="myDropdown" className="ThreeDotMenu-dropdownContent">
+        <a className="ThreeDotMenu-aStyle" key="0" href="#">
           Test
         </a>
-        <a className="aStyle ab" key="1" href="#">
+        <a className="ThreeDotMenu-aStyle" key="1" href="#">
           Update
         </a>
-        <a className="aStyle ac delete" key="2" href="#">
+        <div className="ThreeDotMenu-divider"></div>
+        <a className="ThreeDotMenu-aStyle ThreeDotMenu-destructive" key="2" href="#">
           Delete
         </a>
       </div>
